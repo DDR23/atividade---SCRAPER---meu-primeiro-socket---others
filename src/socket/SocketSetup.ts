9f3685a321a6079d0a7549c2bd3743b5a36c798a
DDR23 <roboreserva.andre@gmail.com>
2024-09-09 20:14:03 -0300

    feat:{subindo estrutura base do projeto}

--- src/socket/SocketSetup.ts
+++ src/socket/SocketSetup.ts
@@ -1,11 +1,14 @@
 import { Server as HttpServer } from 'http';
 import { Server } from 'socket.io';
+import HandleScraper from './handlers/HandleScraper';
 
 export default function SocketSetup(httpServer: HttpServer) {
   const io = new Server(httpServer, { cors: { origin: process.env.URL_BACKEND } });
 
   io.on('connection', (socket) => {
     console.log('Um usuário se conectou');
+    
+    HandleScraper(socket);
 
     socket.on('disconnect', () => console.log('Usuário desconectado'));
   });
