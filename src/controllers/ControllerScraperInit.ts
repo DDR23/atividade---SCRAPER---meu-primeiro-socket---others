ca192f365e43f6a3f3f96e1803478624e978db5c
omegatech-andre <omega.ti_ind@outlook.com>
2024-09-23 14:05:17 -0300

    fix:{ajustes e limpeza de codigo}

--- src/controllers/ControllerScraperInit.ts
+++ src/controllers/ControllerScraperInit.ts
@@ -4,27 +4,23 @@ import { getStartAndFinishTimes, getTimeUntilStart, getTimeRemaining, msToSecond
 import modalStateManager from '../models/ModelStateManager';
 
 export default async function ControllerScraperInit(socket: Socket, data: TypeConfig): Promise<void> {
-  const botId = data._id; // Usando o _id do bot para controlar o estado
+  const botId = data._id;
   modalStateManager.setState(botId, { isRunning: true });
-
   const { startTime, finishTime } = getStartAndFinishTimes(data.CONFIG_TIME_START, data.CONFIG_TIME_FINISH);
-
   try {
     // Loop para verificar o tempo até o início, mas somente se o estado for "isRunning: true"
     while (!isWithinTime(startTime, finishTime)) {
       const currentState = modalStateManager.getState(botId);
       if (!currentState.isRunning) {
-        console.log(`Bot ${botId} foi interrompido antes de iniciar.`);
-        return; // Sai completamente da função se o estado for "isRunning: false"
+        return;
       }
-
       const timeUntilStart = getTimeUntilStart(startTime);
       const secondsUntilStart = msToSeconds(timeUntilStart);
       console.log(`Bot ${botId} fora do horário configurado. Aguardando ${secondsUntilStart} segundos até a inicialização...`);
       await new Promise(resolve => setTimeout(resolve, 1000));
     }
 
-    // ENVIA RESPOSTA DE INÍCIO quando finalmente dentro do horário configurado
+    // ENVIA RESPOSTA DE INÍCIO
     socket.emit('SCRAPER_INIT_RES_ON', {
       title: 'Sucesso',
       message: `Bot ${botId} iniciado!`
@@ -34,25 +30,21 @@ export default async function ControllerScraperInit(socket: Socket, data: TypeCo
 
     // Lógica adicional para o bot rodar durante o período configurado
     while (Date.now() <= finishTime.getTime()) {
-      // Verifica se o estado do bot foi alterado para parar
       const currentState = modalStateManager.getState(botId);
       if (!currentState.isRunning) {
-        console.log(`Bot ${botId} interrompido durante a execução.`);
-        break; // Sai do loop de execução se o estado for "isRunning: false"
+        break;
       }
-
       const timeRemaining = getTimeRemaining(finishTime);
       const secondsRemaining = msToSeconds(timeRemaining);
       console.log(`Bot ${botId} está rodando. Tempo restante: ${secondsRemaining} segundos.`);
       await new Promise(resolve => setTimeout(resolve, 1000));
     }
 
-    // ENVIA RESPOSTA DE TÉRMINO quando o bot terminar o período configurado
+    // ENVIA RESPOSTA DE TÉRMINO
     socket.emit('SCRAPER_INIT_RES_OFF', {
       title: 'Sucesso',
       message: `Bot ${botId} encerrado!`
     });
-
   } catch (error) {
     socket.emit('SCRAPER_INIT_RES', {
       title: 'Erro',
--- src/models/ModelStateManager.ts
+++ src/models/ModelStateManager.ts
@@ -1,28 +1,24 @@
 import { EventEmitter } from 'events';
 
-// Define a interface para o estado de cada bot
 interface BotState {
   isRunning: boolean;
 }
 
-// Gerencia o estado de execução de vários bots simultaneamente
 class ModalStateManager extends EventEmitter {
-  private botsState: Record<string, BotState>; // Armazena o estado de cada bot por ID
+  private botsState: Record<string, BotState>;
 
   constructor() {
     super();
     this.botsState = {};
   }
 
-  // Define o estado de um bot específico
   setState(botId: string, newState: Partial<BotState>): void {
     this.botsState[botId] = { ...this.botsState[botId], ...newState };
-    this.emit('stateChanged', botId, this.botsState[botId]); // Emite um evento com o ID do bot
+    this.emit('stateChanged', botId, this.botsState[botId]);
   }
 
-  // Retorna o estado de um bot específico
   getState(botId: string): BotState {
-    return this.botsState[botId] || { isRunning: true }; // Por padrão, considera o bot rodando
+    return this.botsState[botId] || { isRunning: true };
   }
 }
 
